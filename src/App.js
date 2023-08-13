c741a77edf15e81d9d5f164b30687dbb9f377ac8
yojaesang <js@alterengo.com>
2023-08-14 01:46:00 +0900

    portfolio

--- src/App.js
+++ src/App.js
@@ -5,9 +5,109 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className='font-serif font-bold'>Jae Sang Yoo</h1>
-        <p className="font-serif">hi all. my name is jaesang Yoo</p>
-        <p>2018</p>
+
+        {/* 간격 */}
+        <div className='my-8'></div>
+
+        <h1 className='font-serif'>Jae Sang Yoo</h1>
+      
+        {/* 대학교 이력 */}
+        <div className='text-xl my-4 font-bold'>Education</div>
+        <div className='text-base'>Feb 2012 - Feb 2019</div>
+        <div className='text-base'>Yonsei University</div>
+        <div className='text-base'>Bachelor of Science - BS <br></br> Information & Interaction Design</div>
+        <div className='text-base'>Bachelor of Arts - BA <br></br> Media and Communication</div>
+
+        {/* 간격 */}
+        <div className='my-8'></div>
+
+        {/* 직무 이력 */}
+        <div className='text-xl my-4 font-black'>Experience</div>
+        
+        <div className='text-base'>Sep 2023 - Present</div>
+        <div className='text-base'>Alterengo / Cofounder • Full time</div>
+
+        {/* 간격 */}
+        <div className='my-4'></div>
+
+        <div className='text-base'>Jan 2019 - Sep 2023</div>
+        <div className='text-base'>SK broadband / UIUX Designer • Full time</div>
+
+        {/* 간격 */}
+        <div className='my-2'></div>
+
+
+        <div className='ml-8'>
+          <div className='text-base italic'>Jan - Sep 2023</div>
+          <div className='text-base'>Product Component Technology Squad</div>
+          <ul className='text-base'>
+            <li>B tv App for Apple tv UX Design</li>
+            <li>•  B tv App for Apple tv User Research</li>
+
+            <li>UX Design for Device</li>
+            <li>•  B tv Settings UX for Set-top Boxs & Remote Control</li>
+            <li>•  Remote Pairing UX Enhancement for CUG&#40;Closed User Group&#41;</li>
+          </ul>
+
+        {/* 간격 */}
+        <div className='my-4'></div>
+
+          <div className='text-base italic'>Jan 2019 - Dec 2022</div>
+          <div className='text-base'>Device Development Squad</div>
+          <ul className='text-base'>
+            <li>B tv App for Apple tv UX Design</li>
+            <li>•  B tv App for Apple tv User Research</li>
+
+            <li>UX Design for Device</li>
+            <li>•  B tv Settings UX for Set-top Boxs & Remote Control</li>
+            <li>•  Remote Pairing UX Enhancement for CUG&#40;Closed User Group&#41;</li>
+          </ul>
+        </div>
+
+
+
+
+        {/* 간격 */}
+        <div className='my-8'></div>
+
+        <div className='text-base'>Nov 2017 - Jul 2018</div>
+        <div className='text-base'>Naver Design Camp 2018, Paris, France / UI Dseigner• Intern</div>
+
+
+        {/* 간격 */}
+        <div className='my-8'></div>
+
+        {/* 수상 이력 */}
+        <div className='text-xl my-4 font-black'>Patents</div>
+
+        <div className='text-base'>Nov 2017 - Jul 2018</div>
+        <div className='text-base'>Naver Design Camp 2018, Paris, France</div>
+        <div className='text-base'>Internship</div>
+
+        {/* 간격 */}
+        <div className='my-8'></div>
+
+        <div className='text-base'>Jan 2019 - Sep 2023</div>
+        <div className='text-base'>Naver Design Camp 2018, Paris, France</div>
+        <div className='text-base'>Full time</div>
+
+        {/* 간격 */}
+        <div className='my-8'></div>
+
+        {/* 특허 이력 */}
+        <div className='text-xl my-4 font-black'>Honors & awards</div>
+
+        <div className='text-base'>Nov 2017 - Jul 2018</div>
+        <div className='text-base'>Naver Design Camp 2018, Paris, France</div>
+        <div className='text-base'>Internship</div>
+
+        {/* 간격 */}
+        <div className='my-8'></div>
+
+        <div className='text-base'>Jan 2019 - Sep 2023</div>
+        <div className='text-base'>Naver Design Camp 2018, Paris, France</div>
+        <div className='text-base'>Full time</div>
+
       </header>
     </div>
   );
